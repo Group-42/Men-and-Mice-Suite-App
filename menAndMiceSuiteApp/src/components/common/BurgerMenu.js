90341f218ef46b305be6ab7b69c769987e9f226d
EgillM <egillhalldorsson@gmail.com>
2018-04-17 16:19:16 +0000

    Adds a good UI for the popup menu, and fixes the hamburger

--- menAndMiceSuiteApp/src/components/common/BurgerMenu.js
+++ menAndMiceSuiteApp/src/components/common/BurgerMenu.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import {Image} from 'react-native';
-import {connect} from "react-redux";
-import {Actions} from 'react-native-router-flux';
-import {logoutUser} from "../../actions";
+import React, { Component } from 'react';
+import { Text, Image } from 'react-native';
+import { connect } from "react-redux";
+import { Actions } from 'react-native-router-flux';
+import { logoutUser } from "../../actions";
 import {
     Menu,
     MenuOptions,
@@ -13,20 +13,32 @@ import {
 class BurgerMenu extends Component {
 
     render() {
+        const { burgerStyle, menuStyle, textStyle } = styles;
+
         return(
             <Menu>
                 <MenuTrigger>
                     <Image
-                        style={styles.burgerStyle}
+                        style={ burgerStyle }
                         source={require('../../icons/hamburger.png')}
                     />
                 </MenuTrigger>
-                <MenuOptions>
-                    <MenuOption onSelect={() => Actions.dashboard()} text='Dashboard'/>
-                    <MenuOption onSelect={() => Actions.troubleshoot()} text='Troubleshoot DNS'/>
-                    <MenuOption onSelect={() => Actions.allocate()} text='Allocate IP'/>
-                    <MenuOption onSelect={() => Actions.settings()} text='Settings'/>
-                    <MenuOption onSelect={this.props.logoutUser.bind(this)} text='Log Out'/>
+                <MenuOptions style={ menuStyle }>
+                    <MenuOption onSelect={() => Actions.dashboard()}>
+                        <Text style={ textStyle }> Dashboard </Text>
+                    </MenuOption>
+                    <MenuOption onSelect={() => Actions.troubleshoot()}>
+                        <Text style={ textStyle }> Troubleshoot DNS </Text>
+                    </MenuOption>
+                    <MenuOption onSelect={() => Actions.allocate()}>
+                        <Text style={ textStyle }> Allocate IP </Text>
+                    </MenuOption>
+                    <MenuOption onSelect={() => Actions.settings()}>
+                        <Text style={ textStyle }> Settings </Text>
+                    </MenuOption>
+                    <MenuOption onSelect={this.props.logoutUser.bind(this)}>
+                        <Text style={ textStyle }> Log Out </Text>
+                    </MenuOption>
                 </MenuOptions>
             </Menu>
         );
@@ -40,12 +52,21 @@ const mapStateToProps = ({auth}) => {
 };
 
 const styles = {
+    textStyle: {
+        fontFamily: 'ProximaNova-Semibold',
+        fontSize: 20,
+        color: '#F7B52B'
+    },
+    menuStyle: {
+        backgroundColor: '#19415F',
+        borderWidth: 3,
+        borderColor: '#F7B52B'
+    },
     burgerStyle: {
-        top: 10,
-        right: 10,
+        marginTop: 10,
+        marginRight: 10,
         width: 40,
         height: 40,
-        backgroundColor:'#29495b',
     }
 };
 
